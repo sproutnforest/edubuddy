2fa8623ef52b77e35547587e5a9936f7a9188c6d
briggea <geasandhea19@gmail.com>
2025-04-20 20:29:29 +0700

    fixing server.js

--- server.js
+++ server.js
@@ -157,6 +157,6 @@ app.post('/login', async (req, res) => {
   }
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server running at http://localhost:${port}`);
 });
